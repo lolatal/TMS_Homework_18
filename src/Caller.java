30bf3493e06c627ebc126c0355b17dfde7daf11b
Olga Kemezh <lolatal92@gmail.com>
2021-06-17 11:24:19 +0200

    deadlock example

---
+++ src/Caller.java
@@ -0,0 +1,28 @@
+/**
+ * Two people are trying to call each other. None of them can receive the call unless one of them hangs up. Thus, this can go forever,
+ * while they keep calling each other simultaneously.
+ */
+
+public class Caller {
+    private String name;
+
+    public Caller(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public synchronized void call(Caller executor) {
+
+        System.out.println(executor.getName() + " is calling " + this.name);
+        executor.receiveTheCall(this);
+
+    }
+
+    public synchronized void receiveTheCall(Caller executor) {
+        System.out.println(this.name + ": " + executor.getName() + " has received my call");
+    }
+
+}
